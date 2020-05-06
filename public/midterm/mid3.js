26fbf20849389eefdd4ba5f688163d3549b747e8
Park HyeonJin <hhjj9798@gmail.com>
2020-05-06 10:19:33 +0900

    프론트엔드개발시험 4차 push

--- public/midterm/mid3.js
+++ public/midterm/mid3.js
@@ -3,7 +3,16 @@ for(let i=0; i<3; ++i) {
     a1[i] = [2*i, 2*i+1];
 }
 
-let a2 = Object.assign([ ], a1);
+let a2 = copy(a1);
 a1[0][0] = 100;
 console.log(a1);
 console.log(a2);
+
+function copy(a) {
+    let m = [];
+    for(let i=0; i<a.length; i++) {
+        let t = a[i];
+        m[i] = t;
+    }
+    return m;
+}
