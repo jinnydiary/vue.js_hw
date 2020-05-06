8c8c8f70d3a9a20cb676befe1eaf54cef0e06b3c
Park HyeonJin <hhjj9798@gmail.com>
2020-05-06 11:49:43 +0900

    프론트엔드개발시험 최종 push

--- public/midterm/mid3.js
+++ public/midterm/mid3.js
@@ -3,7 +3,7 @@ for(let i=0; i<3; ++i) {
     a1[i] = [2*i, 2*i+1];
 }
 
-let a2 = Object.assign([], a1);
+let a2 = Object.assign([ ], a1);
 a1[0][0] = 100;
 console.log(a1);
 console.log(a2);
--- public/midterm/mid4.js
+++ public/midterm/mid4.js
@@ -4,6 +4,7 @@ for(let i=0; i<100; ++i) {
     m[i] = Math.floor( Math.random()*(MAX-MIN+1) + MIN);
 }
 
-let count;
+let cnt = 0;
+let count = m.reduce((a) => (a%2==0)? ++cnt : cnt);
 console.log(count);
   
